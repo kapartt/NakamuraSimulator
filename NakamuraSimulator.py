40402be42edd192c4eea043ee0b89e92121fa6e4
kapartt <50311892+kapartt@users.noreply.github.com>
2023-11-27 21:12:13 +0400

    Add files via upload
    uploading simulator

---
+++ NakamuraSimulator.py
@@ -0,0 +1,77 @@
+from random import random
+
+gamesCount = 600
+simulationsCount = 1000
+pointsFracThreshold = 0.985
+minSeriesLen = 35
+maxSeriesLen = 100
+
+winProb = 0.88
+drawProb = 0.04
+loseProb = 1 - winProb - drawProb
+
+seriesSum = 0
+
+
+def getRes(x, w, d):
+    if x < w:
+        return 1
+    
+    if x < w + d:
+        return 0.5
+    
+    return 0
+
+
+nakaSeriesCountOnSimulation = {}
+
+
+for simulationIndex in range(simulationsCount):
+    streakLen = 0
+    streakPts = 0.0
+    nakaSeriesCount = 0
+    sumGamesOnStreaks = 0
+    
+    results = [getRes(random(), winProb, drawProb) for _ in range(gamesCount)]
+    
+    print('Simulation #', (simulationIndex + 1))
+    
+    gameIndex = 0
+    
+    while gameIndex < gamesCount - minSeriesLen:
+        f = False
+        successSeriesLen = 0   
+        successPts = 0
+        newMaxSeriesLen = min(maxSeriesLen, gamesCount - gameIndex)
+        
+        for seriesLen in range(minSeriesLen, newMaxSeriesLen):            
+            streakPts = 0
+            
+            for i in range(seriesLen):
+                streakPts += results[i + gameIndex]
+            
+            if streakPts / seriesLen >= pointsFracThreshold:
+                f = True
+                print('StreakLen = ', seriesLen, ', StreakPts = ', streakPts, ', from game #', (gameIndex + 1), sep='')
+                sumGamesOnStreaks += seriesLen
+                break
+        if f:
+            nakaSeriesCount += 1
+            gameIndex += seriesLen
+        else:
+            gameIndex += 1
+            
+    print('NakaSeriesCount =', nakaSeriesCount)
+    seriesSum += nakaSeriesCount
+    
+    v = nakaSeriesCountOnSimulation.get(nakaSeriesCount, 0)
+    nakaSeriesCountOnSimulation[nakaSeriesCount] = v + 1
+    
+    print('PtsFraction =', sum(results) / gamesCount)
+    print()
+    
+print('Average streaks count =', seriesSum / simulationsCount)
+            
+for x in sorted(nakaSeriesCountOnSimulation):
+    simStr = "simulation" if nakaSeriesCountOnSimulation[x] == 1 else "simulations"
+    print('{0} {1} ({2:.1%}) with {3} streaks'.format(nakaSeriesCountOnSimulation[x], simStr, nakaSeriesCountOnSimulation[x] / simulationsCount, x))
